aaf210fd42f1648149c50950b40dc9b9cb065702
Nikita Valovyy <nickwolf046@gmail.com>
2023-07-16 00:55:30 -0400

    first commit

--- app.js
+++ app.js
@@ -3,16 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth.routes');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.set('trust proxy', 1);
+app.enable('trust proxy');
+
+app.use(
+  session({
+    secret: process.env.SESS_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 60000 
+    }, 
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    })
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,21 +43,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
+app.use('/auth', authRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Project2")
+  .then((connection) =>
+    console.log("connected to " + connection.connection.name)
+  )
+  .catch((err) => console.log(err));
+
 
 module.exports = app;
---
+++ routes/auth.routes.js
@@ -0,0 +1,105 @@
+const {Router} = require('express')
+const mongoose = require('mongoose')
+const User = require("../models/User.model")
+const bcrypt = require("bcryptjs")
+const router = new Router()
+const salt = 12;
+
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
+
+router.get("/signup",isLoggedOut, (req,res) => {
+    res.render('auth/signup.hbs')
+})
+
+
+router.post("/signup", (req, res, next) => {
+    const { username, email, password } = req.body;
+  
+    if (!username || !email || !password) {
+        res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+        return;
+      }
+
+  //     const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  // if (!regex.test(password)) {
+  //   res
+  //     .status(500)
+  //     .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+  //   return;
+  // }
+
+    bcrypt
+      .genSalt(salt)
+      .then((salts) => {
+        return bcrypt.hash(password, salts);
+      })
+      .then((hashedPass) =>
+        User.create({ username, email, passwordHash: hashedPass }).then(
+          (createdUser) => res.redirect("/auth/userProfile")
+        )
+      )
+      .catch(error => {
+        if (error instanceof mongoose.Error.ValidationError) {
+          res.status(500).render('auth/signup', { errorMessage: error.message });
+        } else if (error.code === 11000) {
+   
+          console.log(" Username and email need to be unique. Either username or email is already used. ");
+   
+          res.status(500).render('auth/signup', {
+             errorMessage: 'User already exists.'
+          });
+        } else {
+          next(error);
+        }
+      });
+  });
+
+  router.get("/userProfile",isLoggedIn, (req, res) => {
+
+    res.render('users/user-profile.hbs', { userInSession: req.session.user });
+  });
+
+router.get('/login', (req,res,next) => {
+  res.render('auth/login.hbs')
+})
+
+router.post('/login', (req, res, next) => {
+  console.log('SESSION =====> ', req.session);
+  const { email, password } = req.body;
+ 
+  if (email === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        console.log("Email not registered. ");
+        res.render('auth/login', { errorMessage: 'User not found and/or incorrect password.' });
+        return;
+      } else if (bcrypt.compareSync(password, user.passwordHash)) {
+        
+        req.session.user = user;
+
+        console.log("Session AFTER login", req.session)
+
+        res.redirect("/auth/userProfile")
+      } else {
+        console.log("Incorrect password. ");
+        res.render('auth/login', { errorMessage: 'User not found and/or incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.redirect('/auth/login');
+  });
+});
+
+module.exports = router;
