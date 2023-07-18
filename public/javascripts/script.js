82a4835fd8eedfb48fcd38254b6d0c5b910080fa
Nikita Valovyy <nickwolf046@gmail.com>
2023-07-17 20:56:39 -0400

    map

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,44 @@
 window.onload = () => {
-    let mexButton = document.getElementById('mexicobut')
-    let mexDiv = document.getElementById('mexico')
-    mexButton.onclick = () => {
-        mexDiv.style.backgroundColor = "purple"
-        console.log("clicking!!!!!")
+    function applyAnimation(element) {
+        element.style.animation = "do 1.5s ease-in infinite";
+        element.style.border = "solid silver";
+        element.style.borderRadius = "100%";
+        element.style.content = "";
     }
+    
+    function removeAnimation(element) {
+        element.style.animation = "";
+        element.style.border = "none";
+        element.style.borderRadius = "";
+    }
+    
+    function setupButtonAnimation(buttonId, divId) {
+        const button = document.getElementById(buttonId);
+        const div = document.getElementById(divId);
+    
+        button.addEventListener('mouseover', () => applyAnimation(div));
+        button.addEventListener('mouseout', () => removeAnimation(div));
+    }
+    
+    const buttonDivPairs = [
+        ['mexicobut', 'mexico'],
+        ['miamibut', 'miami'],
+        ['racoonbut', 'racoon'],
+        ['denverbut', 'denver'],
+        ['catarinabut', 'catarina'],
+        ['brasilbut', 'brasil'],
+        ['lyonbut', 'lyon'],
+        ['berlinbut', 'berlin'],
+        ['kyivbut', 'kyiv'],
+        ['capebut', 'cape'],
+        ['delhibut', 'delhi'],
+        ['shanghaibut', 'shanghai'],
+        ['tokyobut', 'tokyo'],
+        ['sydneybut', 'sydney'],
+    ];
+    
+    buttonDivPairs.forEach(pair => {
+        const [buttonId, divId] = pair;
+        setupButtonAnimation(buttonId, divId);
+    });
 }
